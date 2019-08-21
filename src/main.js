b4ee48e19eb9ec47c0d5c5daa11ee33ba267efdc
ll <1096195574@qq.com>
2019-08-21 23:44:58 +0800

    mint-ui

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueResource from 'vue-resource'
+import {Button} from 'mint-ui';
+import 'mint-ui/lib/style.css'
 
-//声明使用插件
-Vue.use(VueResource) // 内部会给vm对象和组件对象添加一个属性: $http
 
+// 注册成标签(全局注册，所有其他主键都可以使用)
+Vue.component(Button.name, Button);  //每一个主键都有一个属性代表 'mt-button' Button.name
 new Vue({
   el: '#app',
   components: {
